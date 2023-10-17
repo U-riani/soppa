9c19597ecfcea5a940c66f2157065316547787f2
U-riani <sand.papi@yahoo.com>
2023-10-17 21:53:03 +0400

    second 1

---
+++ script.js
@@ -0,0 +1,64 @@
+const dotsContainer = document.querySelector(".dots-container");
+const homePage1 = document.querySelector(".home-page-1");
+let determineSlidesMove = 0;
+const dots = document.querySelectorAll(".dot");
+
+// Separate images on home page 1
+document.querySelectorAll(".home-page-1-img-cont").forEach((el, i) => {
+  el.style.transform = `translateX(${100 * i}%)`;
+});
+
+// Set first dot to black
+document.querySelector(".dot-1").classList.add("active");
+
+// Change slides and dots automaticaly
+const intervalCallback = function () {
+  // Control dots flow
+  dots.forEach((el, i) => {
+    el.classList.remove("active");
+    console.log('active')
+  });
+
+  if (determineSlidesMove > 6) {
+    determineSlidesMove = 0;
+  }
+  
+  determineSlidesMove++;
+  document.querySelectorAll(".home-page-1-img-cont").forEach((el, i) => {
+    el.style.transform = `translateX(${100 * (i + 1 - determineSlidesMove)}%)`;
+    if (el.style.transform === "translateX(0%)") {
+      document.querySelector(`.dot-${i + 1}`).classList.add("active");
+      console.log("yup");
+    }
+  });
+};
+
+const slideInterval = setInterval(intervalCallback, 2000);
+// slideInterval();
+
+setTimeout(() => {
+  clearInterval(slideInterval);
+  console.log("time out");
+}, 30000);
+
+// CHnage dots color when clicked
+dotsContainer.addEventListener("click", function (e) {
+  if (e.target.dataset.num) {
+    document.querySelectorAll(".dot").forEach((dot) => {
+      dot.classList.remove('active');
+    });
+
+    let countSlide = e.target.dataset.num;
+    determineSlidesMove = countSlide;
+    document.querySelectorAll(".home-page-1-img-cont").forEach((el, i) => {
+      el.style.transform = `translateX(${100 * (i + 1 - countSlide)}%)`;
+    });
+
+    e.target.classList.add('active');
+
+    // const container1 = document.querySelector('.container-1');
+    // container1.style.backgroundColor = 'grey';
+    // container1.style.background = `url(images/home-${e.target.dataset.num}.png)`;
+    // container1.style.backgroundSize = `cover`;
+  }
+});
