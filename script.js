ff0f9f942ff648e1d0e7c2b98ae0a3c8c3311e8d
U-riani <sand.papi@yahoo.com>
2023-10-18 21:24:31 +0400

    third

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const homePage1 = document.querySelector(".home-page-1");
 let determineSlidesMove = 0;
 const dots = document.querySelectorAll(".dot");
 
+
 // Separate images on home page 1
 document.querySelectorAll(".home-page-1-img-cont").forEach((el, i) => {
   el.style.transform = `translateX(${100 * i}%)`;
@@ -16,36 +17,34 @@ const intervalCallback = function () {
   // Control dots flow
   dots.forEach((el, i) => {
     el.classList.remove("active");
-    console.log('active')
   });
 
   if (determineSlidesMove > 6) {
     determineSlidesMove = 0;
   }
-  
+
   determineSlidesMove++;
   document.querySelectorAll(".home-page-1-img-cont").forEach((el, i) => {
     el.style.transform = `translateX(${100 * (i + 1 - determineSlidesMove)}%)`;
     if (el.style.transform === "translateX(0%)") {
       document.querySelector(`.dot-${i + 1}`).classList.add("active");
-      console.log("yup");
     }
   });
 };
 
-const slideInterval = setInterval(intervalCallback, 2000);
+const slideInterval = setInterval(intervalCallback, 4000);
 // slideInterval();
 
-setTimeout(() => {
-  clearInterval(slideInterval);
-  console.log("time out");
-}, 30000);
+// Remove interval when user refresh page or leave it
+window.addEventListener("beforeunload", function (e) {
+  this.clearInterval(slideInterval);
+});
 
 // CHnage dots color when clicked
 dotsContainer.addEventListener("click", function (e) {
   if (e.target.dataset.num) {
     document.querySelectorAll(".dot").forEach((dot) => {
-      dot.classList.remove('active');
+      dot.classList.remove("active");
     });
 
     let countSlide = e.target.dataset.num;
@@ -54,11 +53,6 @@ dotsContainer.addEventListener("click", function (e) {
       el.style.transform = `translateX(${100 * (i + 1 - countSlide)}%)`;
     });
 
-    e.target.classList.add('active');
-
-    // const container1 = document.querySelector('.container-1');
-    // container1.style.backgroundColor = 'grey';
-    // container1.style.background = `url(images/home-${e.target.dataset.num}.png)`;
-    // container1.style.backgroundSize = `cover`;
+    e.target.classList.add("active");
   }
 });
